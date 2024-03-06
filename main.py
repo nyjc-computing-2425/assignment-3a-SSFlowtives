50fe52f2604ded01969be0330a2d9ce25477dbde
Manuel <112157396+SSFlowtives@users.noreply.github.com>
2024-03-06 07:08:58 +0000

    Updated assignment 3a

--- main.py
+++ main.py
@@ -6,25 +6,21 @@ numbers = nric[1:8]
 suffix = nric[8:].upper()
 bool = False
 
-dictST = {
-  'J' : 0, 'Z' : 1, 'I' : 2, 'H' : 3, 'G' : 4, 'F' : 5, 'E' : 6, 'D' : 7, 'C' : 8, 'B' : 9, 'A' : 10
-}
-dictFG = {
-  'X' : 0, 'W' : 1, 'U' : 2, 'T' : 3, 'R' : 4, 'Q' : 5, 'P' : 6, 'N' : 7, 'M' : 8, 'L' : 9, 'K' : 10
-}
+dictST = "JZIHGFEDCBA"
+dictFG = "XWUTRQPNMLK"
 
-if prefix in ['S', 'T', 'F', 'G']:
+if prefix in "STFG":
   if numbers.isdigit():
     if len(suffix) == 1 and suffix.isalpha():
-      check = 2 * int(nric[1]) + 7 * int(nric[2]) + 6 * int(nric[3]) + 5 * int(nric[4]) + 4 * int(nric[5]) + 3 * int(nric[6]) + 2 * int(nric[7])
-      if prefix in ['T', 'G']:
+      check = 2*int(nric[1]) + 7*int(nric[2]) + 6*int(nric[3]) + 5*int(nric[4]) + 4*int(nric[5]) + 3*int(nric[6]) + 2*int(nric[7])
+      if prefix in "TG":
         check += 4
       check %= 11
-      if prefix in ['S', 'T']:
-        if dictST[suffix] == check:
+      if prefix in "ST":
+        if dictST[check] == suffix:
           bool = True
       else:
-        if dictFG[suffix] == check:
+        if dictFG[check] == suffix:
           bool = True
 
 if bool:
